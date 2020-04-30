9ea82f5f0d3d91c45ca266d81afa42381c21004a
Randy Duodu <52050586+iSOLveIT@users.noreply.github.com>
2020-04-30 19:37:44 +0000

    Update __init__.py

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 
 # Config and Instantiate Mongo
-Username = urllib.parse.quote_plus('isolveit')
-Password = urllib.parse.quote_plus('laden1472')
+Username = urllib.parse.quote_plus('USERNAME')
+Password = urllib.parse.quote_plus('PASSWORD')
 
-app.config['MONGO_URI'] = "mongodb+srv://%s:%s@isolveit-jzamv.mongodb.net/url_shortener" % (Username, Password)
-app.config['MONGO_DB'] = "url_shortener"
+app.config['MONGO_URI'] = "mongodb+srv://%s:%s@example.mongodb.net/DB_NAME" % (Username, Password)
+# Name of Database to connect to
+app.config['MONGO_DB'] = "DB_NAME"
 
 # connect to MongoDB with school_DB
 mongo = PyMongo(app)
